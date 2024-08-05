e62c107573f1c1ad79a59320e1a138e0ce7513f6
Sm4cker <121897314+Sm4cker@users.noreply.github.com>
2024-08-05 17:40:37 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
         <nav>
-            <img alt="Logo" src=".../images/airbnb-logo.png" className="nav--logo" />
+            <img alt="Logo" src={process.env.PUBLIC_URL + '/images/airbnb-logo.png'} className="nav--logo" />
         </nav>
     )
 }
